c4e320283a53777f2cd083959605e4a7aaf6a5d8
Dana Varahi <dana_varahi@protonmail.com>
2020-10-16 13:36:32 +0100

    added increase_pets_sold

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -4,7 +4,6 @@ def get_pet_shop_name(pet_shop):
 
 
 def get_total_cash(pet_shop):
-
     return pet_shop['admin']['total_cash']
 
 
@@ -14,5 +13,9 @@ def add_or_remove_cash(pet_shop, amount):
 
 
 def get_pets_sold(pet_shop):
+    return pet_shop['admin']['pets_sold']
 
+
+def increase_pets_sold(pet_shop, amount):
+    pet_shop['admin']['pets_sold'] += amount
     return pet_shop['admin']['pets_sold']
