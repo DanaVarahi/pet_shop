78753968a2e88d33e64c6ea99b74edad768d6900
Dana Varahi <dana_varahi@protonmail.com>
2020-10-16 16:54:07 +0100

    Added remove_pet_by_name

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -39,8 +39,13 @@ def get_pets_by_breed(pet_shop, breed):
 
 
 def find_pet_by_name(pet_shop, name):
-    # pdb.set_trace()
 
     for pet in pet_shop['pets']:
         if pet['name'] == name:
             return pet
+
+
+def remove_pet_by_name(pet_shop, name):
+    for pet in pet_shop['pets']:
+        if pet_shop['pets']['name'] == name:
+            del pet
